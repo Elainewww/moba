37ed2e406b8f86cc497fdd43b809d6fe4effec72
Elaine Wu <wuww1114@163.com>
2019-08-29 22:05:12 +0800

    hero-model

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -3,6 +3,29 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   name: { type: String },
   avatar: { type: String },
+  title: { type: String },
+  categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+  scores: {
+    difficult: { type: Number },
+    skills: { type: Number },
+    attack: { type: Number },
+    survive: { type: Number },
+  }, //对象：这里通过对象取值比通过数组的下标取值方便
+  skills: [{
+    icon: { type: String },
+    name: { type: String },
+    description: { type: String },
+    tips: { type: String },
+  }],
+  items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Items' }],
+  items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Items' }],
+  usageTips: { type: String },
+  battleTips: { type: String },
+  teamTips: { type: String },
+  partners: [{
+    hero: { ype: mongoose.SchemaTypes.ObjectId, ref: 'Hero' },
+    description: { type: String }
+  }],
 })
 
 module.exports = mongoose.model('Hero', schema)
